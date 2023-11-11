69aa4300e9c08a036642e650b3580a13e98e96c2
Mohamad Agung Faisal <agoenkf97@gmail.com>
2023-11-11 18:59:28 +0700

    add layout on register

---
+++ app/register/layout.jsx
@@ -0,0 +1,34 @@
+import '@/app/globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Nunito } from 'next/font/google'
+import { dark } from '@clerk/themes'
+import Navbar from '@/components/Navbar/page'
+
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Restaurants',
+  description: 'Sekawan Test',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={nunito.className}>
+          <Navbar />
+          <main className='container mx-auto'>
+            <div className='flex items-start justify-center min-h-screen'>
+              <div className='mt-20'>{children}</div>
+            </div>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
